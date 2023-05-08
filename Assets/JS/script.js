fe76c912a7c75268dbe7add5fceb1ef1b90f81db
nicco <mannythemanmoth@gmail.com>
2023-05-08 14:31:42 -0400

    created method to request url to take city parameter from input field

---
+++ Assets/JS/script.js
@@ -0,0 +1,31 @@
+var searchEl = $('#city-input');
+var searching = $('#search');
+var city = 'Boston' ;
+let units = 'imperial';
+const apiKey = "3844c5bd7e6c5d634485a1c3050b8f72&"
+const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?';
+// var requestURL = ''
+var requestURL = apiUrl + 'q=' + city + '&appid=' + apiKey + units
+// console.log(requestURL)
+//on button press
+function searchSubmit (event){
+    event.preventDefault();
+    city = $("#city-input").val();
+    console.log(city)
+    requestURL = apiUrl + 'q=' + city + '&appid=' + apiKey + units;
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            
+        })
+    
+    
+
+};
+searching.on("click", searchSubmit);
+console.log(city);
+console.log(requestURL);
+
