dd0fc670ca646ed2a3107323d841ea108e87345c
nicco <mannythemanmoth@gmail.com>
2023-05-11 23:54:18 -0400

    added icons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,6 +8,7 @@ var rForecast;
 var rWeather;
 const apiKey = "3844c5bd7e6c5d634485a1c3050b8f72";
 const apiUrl = 'https://api.openweathermap.org/data/2.5/';
+const iconurl = 'https://openweathermap.org/img/wn/';
 //5 day forecast
 var fore = 'forecast?'
 //current weather
@@ -22,8 +23,14 @@ function searchSubmit(event) {
     forecastEl.html("");
     city = $("#city-input").val();
     // console.log(city)
-    //todays weather forecast
-    weatRequestURL = apiUrl + weat + 'q=' + city + '&units=' + units + '&appid=' + apiKey;
+    previous = $('prevSearch')
+    searchedBtn = $('<button>')
+    searchedBtn.text(city)
+    searchedBtn.addClass('searched')
+    searchedBtn.attr('id', )
+    // searchedBtn.on('click', '.searched')
+        //todays weather forecast
+        weatRequestURL = apiUrl + weat + 'q=' + city + '&units=' + units + '&appid=' + apiKey;
     console.log(weatRequestURL);
     fetch(weatRequestURL)
         .then(function (response2) {
@@ -32,16 +39,22 @@ function searchSubmit(event) {
         .then(function (data2) {
             //addtional function creation stuff
             rWeather = data2;
-            rWeather.
+            console.log(rWeather)
+            curIcon = iconurl + rWeather.weather[0].icon+"@2x.png"
+            console.log(curIcon);
             dayTitle = $('<div>');
-            todays = $("<div>");
-            todays.addClass('col-12 col-md-6 col-lg-2 card');
-            todaysEl.append(todays);
-            todaysTemp = rWeather.main.temp
-            todaysHumitdy = rWeather.main.humidity
-            todaysWind = rWeather.wind.speed
-
-        })
+            dayTitle.text("Today's Weather");
+            todaysTemp = $("<div>");
+            todaysHumitdy = $('<div>')
+            todaysWind = $('<div>');
+            todaysIcon = $('<img>')
+            todaysIcon.attr('src', curIcon)
+            todaysEl.addClass('col-12 col-md-6 col-lg-2 card');
+            todaysTemp.text("Temp: "+ rWeather.main.temp + " F")
+            todaysHumitdy.text("Humidity: "+rWeather.main.humidity)
+            todaysWind.text("Wind: "+rWeather.wind.speed +" mph")
+            todaysEl.append(dayTitle, todaysTemp, todaysHumitdy, todaysWind, todaysIcon);
+        })  
 
     forecastRequestURL = apiUrl + fore + 'q=' + city + '&units=' + units + '&appid=' + apiKey;
     fetch(forecastRequestURL)
@@ -52,34 +65,39 @@ function searchSubmit(event) {
             // 5 day forecast fetch functional stuff
             console.log(data1)
             rForecast = data1;
-            var i =0
-            for (let x = 0; x < 5; x++) {   
-                createCard(x,i)
-                i+=8
+            var i = 0
+            for (let x = 0; x < 5; x++) {
+                createCard(x, i)
+                i += 8
             }
         })
 };
-function createCard(x,i) {
+function createCard(x, i) {
     //add container div
-    var cur = $('#day '+x);
+    var cur = $('#day ' + x);
     var titl = "day" + x;
     var containerEl = $('<div>');
     containerEl.addClass('col-12 col-md-6 col-lg-2 card');
-     //add card class and dayID from x
+    //add card class and dayID from x
     containerEl.attr('id', titl);
     forecastEl.append(containerEl);
     var Dte = $('<h4>');
     var temperature = $('<h4>');
     var Humid = $('<h4>');
     var windy = $('<h4>');
-    var iconic = $('<h4>');
+    var iconic = $('<img>');
+    foreIcon = iconurl + rForecast.list[i].weather[0.icon]+"@2x.png";
+    iconic.attr('src', foreIcon)
     tdt = new Date(rForecast.list[i].dt * 1000).toLocaleDateString("en-US")
-     temperature.text = rForecast.list[i].main.temp;
-     Humid.text(rForecast.list[i].main.humidity);
+    temperature.text(rForecast.list[i].main.temp);
+    Humid.text(rForecast.list[i].main.humidity);
     windy.text(rForecast.list[i].wind.speed);
-     Dte.text (tdt);
+    Dte.text(tdt);
+    
+    // iconic.attr('src','curicon')
+
     //  iconic.text(rForecast.list[i].main.)
-     containerEl.append(Dte, temperature, Humid, windy);
+    containerEl.append(Dte, temperature, Humid, windy);
 
 
 }
